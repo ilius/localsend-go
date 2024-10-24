faa4e423363995a8c6688378a82d9ded85288abb
Saeed Rasooli <saeed.gnu@gmail.com>
2024-10-24 13:28:02 +0330

    handle error from flagSet.Parse

--- cmd/localsend-go/main.go
+++ cmd/localsend-go/main.go
@@ -30,7 +30,11 @@ func main() {
 	)
 	filePath := flagSet.String("file", "", "Path to the file to upload")
 	toDevice := flagSet.String("to", "", "Send file to Device ip,Write device receiver ip here")
-	flagSet.Parse(os.Args[1:])
+	err := flagSet.Parse(os.Args[1:])
+	if err != nil {
+		os.Stderr.WriteString(err.Error() + "\n")
+		os.Exit(1)
+	}
 
 	switch *mode {
 	case cmd_send:
